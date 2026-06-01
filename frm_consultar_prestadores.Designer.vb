653f23eed9f8a73ff58bd7099bf0c8fe068d4350
Kayllon Lemos <89818136+kayllonlemos@users.noreply.github.com>
2026-05-31 21:26:04 -0300

    form consultar prestadores
    bug do modo design corrigido

---
+++ frm_consultar_prestadores.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_consultar_prestadores
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_consultar_prestadores))
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel9 = New System.Windows.Forms.Panel()
+        Me.dgv_info = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.lbl_frm_contas = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.cmb_especialidade = New System.Windows.Forms.ComboBox()
+        Me.lbl_especialidade = New System.Windows.Forms.Label()
+        Me.cmb_prestador = New System.Windows.Forms.ComboBox()
+        Me.lbl_prestador = New System.Windows.Forms.Label()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.Panel9.SuspendLayout()
+        CType(Me.dgv_info, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Panel2.BackColor = System.Drawing.Color.Black
+        Me.Panel2.Controls.Add(Me.Panel1)
+        Me.Panel2.Location = New System.Drawing.Point(-1, -4)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Panel2.Size = New System.Drawing.Size(857, 591)
+        Me.Panel2.TabIndex = 6
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.Panel9)
+        Me.Panel1.Controls.Add(Me.lbl_frm_contas)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Location = New System.Drawing.Point(7, 11)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Panel1.Size = New System.Drawing.Size(843, 571)
+        Me.Panel1.TabIndex = 1
+        '
+        'Panel9
+        '
+        Me.Panel9.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.Panel9.Controls.Add(Me.dgv_info)
+        Me.Panel9.Location = New System.Drawing.Point(38, 184)
+        Me.Panel9.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel9.Name = "Panel9"
+        Me.Panel9.Size = New System.Drawing.Size(767, 325)
+        Me.Panel9.TabIndex = 31
+        '
+        'dgv_info
+        '
+        Me.dgv_info.AllowUserToAddRows = False
+        Me.dgv_info.AllowUserToDeleteRows = False
+        Me.dgv_info.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.dgv_info.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgv_info.BackgroundColor = System.Drawing.Color.White
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(26, Byte), Integer))
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(217, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(26, Byte), Integer))
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgv_info.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgv_info.ColumnHeadersHeight = 29
+        Me.dgv_info.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.dgv_info.Location = New System.Drawing.Point(11, 11)
+        Me.dgv_info.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.dgv_info.Name = "dgv_info"
+        Me.dgv_info.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.dgv_info.RowTemplate.Height = 24
+        Me.dgv_info.Size = New System.Drawing.Size(747, 303)
+        Me.dgv_info.TabIndex = 25
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "ID"
+        Me.Column1.MinimumWidth = 6
+        Me.Column1.Name = "Column1"
+        Me.Column1.Visible = False
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Nome"
+        Me.Column2.MinimumWidth = 6
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "E-mail"
+        Me.Column3.MinimumWidth = 6
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Tipo Prestador"
+        Me.Column4.MinimumWidth = 6
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Endereço"
+        Me.Column5.MinimumWidth = 6
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Telefone"
+        Me.Column6.MinimumWidth = 6
+        Me.Column6.Name = "Column6"
+        '
+        'lbl_frm_contas
+        '
+        Me.lbl_frm_contas.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_frm_contas.Font = New System.Drawing.Font("JejuMyeongjo", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_frm_contas.Location = New System.Drawing.Point(239, 21)
+        Me.lbl_frm_contas.Name = "lbl_frm_contas"
+        Me.lbl_frm_contas.Size = New System.Drawing.Size(383, 55)
+        Me.lbl_frm_contas.TabIndex = 24
+        Me.lbl_frm_contas.Text = "Consultar Prestadores:"
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(308, -100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(216, 47)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Bem vindo(a)..."
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Black
+        Me.Panel3.Controls.Add(Me.cmb_especialidade)
+        Me.Panel3.Controls.Add(Me.lbl_especialidade)
+        Me.Panel3.Controls.Add(Me.cmb_prestador)
+        Me.Panel3.Controls.Add(Me.lbl_prestador)
+        Me.Panel3.Controls.Add(Me.ToolStrip1)
+        Me.Panel3.Location = New System.Drawing.Point(0, 76)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(843, 57)
+        Me.Panel3.TabIndex = 30
+        '
+        'cmb_especialidade
+        '
+        Me.cmb_especialidade.FormattingEnabled = True
+        Me.cmb_especialidade.Location = New System.Drawing.Point(583, 16)
+        Me.cmb_especialidade.Name = "cmb_especialidade"
+        Me.cmb_especialidade.Size = New System.Drawing.Size(175, 24)
+        Me.cmb_especialidade.TabIndex = 31
+        '
+        'lbl_especialidade
+        '
+        Me.lbl_especialidade.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.lbl_especialidade.Font = New System.Drawing.Font("Bpmf Huninn", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_especialidade.Location = New System.Drawing.Point(423, 14)
+        Me.lbl_especialidade.Name = "lbl_especialidade"
+        Me.lbl_especialidade.Size = New System.Drawing.Size(199, 23)
+        Me.lbl_especialidade.TabIndex = 30
+        Me.lbl_especialidade.Text = "Especialidade:"
+        '
+        'cmb_prestador
+        '
+        Me.cmb_prestador.FormattingEnabled = True
+        Me.cmb_prestador.Location = New System.Drawing.Point(138, 16)
+        Me.cmb_prestador.Name = "cmb_prestador"
+        Me.cmb_prestador.Size = New System.Drawing.Size(175, 24)
+        Me.cmb_prestador.TabIndex = 29
+        '
+        'lbl_prestador
+        '
+        Me.lbl_prestador.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.lbl_prestador.Font = New System.Drawing.Font("Bpmf Huninn", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_prestador.Location = New System.Drawing.Point(13, 14)
+        Me.lbl_prestador.Name = "lbl_prestador"
+        Me.lbl_prestador.Size = New System.Drawing.Size(199, 23)
+        Me.lbl_prestador.TabIndex = 28
+        Me.lbl_prestador.Text = "Prestador:"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ToolStrip1.AutoSize = False
+        Me.ToolStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(194, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.None
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStrip1.Location = New System.Drawing.Point(-7, 5)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(857, 47)
+        Me.ToolStrip1.TabIndex = 26
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'frm_consultar_prestadores
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(855, 587)
+        Me.Controls.Add(Me.Panel2)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frm_consultar_prestadores"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Consultar Prestadores:"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel9.ResumeLayout(False)
+        CType(Me.dgv_info, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents dgv_info As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents lbl_frm_contas As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents cmb_especialidade As ComboBox
+    Friend WithEvents lbl_especialidade As Label
+    Friend WithEvents cmb_prestador As ComboBox
+    Friend WithEvents lbl_prestador As Label
+    Friend WithEvents ToolStrip1 As ToolStrip
+End Class
---
+++ frm_consultar_prestadores.vb
@@ -0,0 +1,31 @@
+﻿Public Class frm_consultar_prestadores
+    Private Sub frm_consultar_prestadores_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Conectar_banco()
+        Carregar_prestadoresConsulta()
+        Carregar_especialidadesConsulta()
+        Dados_DataGridViewConsultaPrestadores()
+    End Sub
+
+    Private Sub cmb_prestador_LostFocus(sender As Object, e As EventArgs) Handles cmb_prestador.LostFocus
+        aux = cmb_prestador.Text
+        If cmb_prestador.Text = "" And cmb_especialidade.Text = "" Then
+            Dados_DataGridViewConsultaPrestadores()
+        ElseIf cmb_especialidade.Text = "" Then
+            Filtrar_DataGridView_ConsultaPrestador()
+        Else
+            Filtrar_DataGridView_Consulta()
+        End If
+    End Sub
+
+    Private Sub cmb_especialidade_LostFocus(sender As Object, e As EventArgs) Handles cmb_especialidade.LostFocus
+        aux2 = cmb_especialidade.Text
+        If cmb_especialidade.Text = "" And cmb_prestador.Text = "" Then
+            Dados_DataGridViewConsultaPrestadores()
+        ElseIf cmb_prestador.Text = "" Then
+            Filtrar_DataGridView_ConsultaEspecialidade()
+        Else
+            Filtrar_DataGridView_Consulta()
+        End If
+    End Sub
+
+End Class
